f2dd0183ff1f5506f7f847215e4db1590a8a3948
Paige Sardzinski <mpsardz@live.unc.edu>
2021-05-26 10:31:32 -0400

    fortune cookies

--- exercises/ex03/fortune_cookie.py
+++ exercises/ex03/fortune_cookie.py
@@ -13,3 +13,19 @@ from random import randint
 
 
 # Begin your solution here...
+
+
+print("Your fortune cookie says...")
+
+x = randint(1,4)
+
+if x == 1:
+    print("You will have the best day ever!")
+elif x == 2:
+    print ("There is a wonderful surprsing waiting for you around the corner!")
+elif x == 3:
+    print ("You will soon accomplish something great!")
+else:
+    print("Life is about to get a little twisted!")
+
+print("Now, go spread positive vibes!")
