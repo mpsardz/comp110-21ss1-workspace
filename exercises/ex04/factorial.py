d8014d62c1158d8dc243aaca9667611dc0b14679
Paige Sardzinski <mpsardz@live.unc.edu>
2021-06-04 16:31:28 -0500

    ex04

--- exercises/ex04/factorial.py
+++ exercises/ex04/factorial.py
@@ -1,6 +1,16 @@
 """An exercise in computing the factorial of an int."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730004269"
 
 
 # Begin your solution here...
+
+some_int: int = int(input("Choose a number: "))
+
+i: int = 1
+total: int = 1
+while i <= some_int:
+    total *= i
+    i += 1
+
+print("Factorial: " + str(total))
--- exercises/ex04/repeat_beat.py
+++ exercises/ex04/repeat_beat.py
@@ -1,6 +1,16 @@
 """Repeating a beat in a loop."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730004269"
 
 
 # Begin your solution here...
+beat: str = str(input("What beat do you want to repeat? "))
+
+repeat: int = int(input("How many times do you want to repeat it? "))
+
+i: int = 1
+while i <= repeat:
+    print(beat)
+    i += 1
+if repeat <= 1:
+    print("No beat...")
